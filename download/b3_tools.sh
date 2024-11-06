4f89b5b832b3f7023e300fad451440e53b9b9c03
buzhihuigai@hotmail.com <buzhihuigai@hotmail.com>
2024-11-06 11:21:22 +0800

    增加工具下载脚本

---
+++ download/b3_tools.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://172.16.44.106:9000/self-packet/h3c/oe2203sp1/x86_64/h3c-tool-latest.oe2203sp1.x86_64.rpm
+wget http://172.16.44.106:9000/self-packet/h3c/oe2203sp1/aarch64/h3c-tool-latest.oe2203sp1.aarch64.rpm
+wget http://172.16.44.106:9000/self-packet/h3c/ky10/x86_64/h3c-tool-latest.ky10.x86_64.rpm
+wget http://172.16.44.106:9000/self-packet/h3c/ky10/aarch64/h3c-tool-latest.ky10.aarch64.rpm
